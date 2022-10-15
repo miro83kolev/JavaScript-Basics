c250aae50833d2069b9d686c087ce8c8ea4e1ebe
miro83kolev <miro83kolev@gmail.com>
2022-10-15 16:17:40 +0300

    update

--- .vscode/exam_prep/travelAgency.js
+++ .vscode/exam_prep/travelAgency.js
@@ -6,33 +6,217 @@ function travelAgency(input){
     let daysOfStay = Number(input[3])
 
     let pricePerDay = 0
-    let finalPrice = 0
+    let finalPrice = 0   
 
-    if (town === 'Bansko' || town === 'Borovets'){
-        if (packageType == 'withEquipment' && vipOrNot === 'no'){
-            if (daysOfStay < 1){
-                console.log('Days must be positive number!')
-            }else if (daysOfStay > 7){
-                pricePerDay = 100
-                finalPrice = (pricePerDay * daysOfStay - 1) 
-                console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
-            }else{
-                pricePerDay = 100
-                finalPrice = pricePerDay * daysOfStay
-                console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+    switch (town){
+        
+        case 'Bansko':
+            switch (packageType){
+                case 'noEquipment':
+                    if (vipOrNot === 'yes'){
+                        pricePerDay = 80
+                        pricePerDay *= 0.95
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }else{
+                        pricePerDay = 80
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }                        
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }
+                    break
+                case 'withEquipment':
+                    if (vipOrNot === 'yes'){
+                        pricePerDay = 100
+                        pricePerDay *= 0.90
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }else{
+                        pricePerDay = 100  
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }                     
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }
+                    break                    
+                default:
+                    console.log(`Invalid input!`)
+                    break
+            }
+            break
+        case 'Borovets':
+            switch (packageType){
+                case 'noEquipment':
+                    if (vipOrNot === 'yes'){
+                        pricePerDay = 80
+                        pricePerDay *= 0.95
+                        if (daysOfStay < 1){
+                            console.log(`Days must be positive number!`)
+                            break
+                        }else{
+                            finalPrice = daysOfStay * pricePerDay
+                            console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                        }                        
+                    }else{
+                        pricePerDay = 80
+                        if (daysOfStay < 1){
+                            console.log(`Days must be positive number!`)
+                            break
+                        }else{
+                            finalPrice = daysOfStay * pricePerDay
+                            console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                        }                       
+                    }
+                    break             
+                    
+                case 'withEquipment':
+                    if (vipOrNot === 'yes'){
+                        pricePerDay = 100
+                        pricePerDay *= 0.90
+                        if (daysOfStay < 1){
+                            console.log(`Days must be positive number!`)
+                            break
+                        }else{
+                            finalPrice = daysOfStay * pricePerDay
+                            console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                        }  
+                       
+                    }else{
+                        pricePerDay = 100
+                        if (daysOfStay < 1){
+                            console.log(`Days must be positive number!`)
+                            break
+                        }else{
+                            finalPrice = daysOfStay * pricePerDay
+                            console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                        }                       
+                        
+                    }
+                    break
+                default:
+                    console.log(`Invalid input!`)
+                    break
+            }
+            break
+        case 'Varna':
+            switch (packageType){
+                case 'noBreakfast':
+                    if (vipOrNot === 'yes'){
+                        pricePerDay = 100
+                        pricePerDay *= 0.93
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }else{
+                        pricePerDay = 100
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }                       
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }
+                    break              
+                    
+                case 'withBreakfast':
+                    if (vipOrNot === 'yes'){
+                        pricePerDay = 130
+                        pricePerDay *= 0.88
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }else{
+                        pricePerDay = 130
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }                       
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }
+                    break
+                default:
+                    console.log(`Invalid input!`)
+                    break
+            }
+            break
+        case 'Burgas':
+            switch (packageType){
+                case 'noBreakfast':
+                    if (vipOrNot === 'yes'){
+                        pricePerDay = 100
+                        pricePerDay *= 0.93
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }else{
+                        pricePerDay = 100 
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }                       
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }
+                    break              
+                    
+                case 'withBreakfast':
+                    if (vipOrNot === 'yes'){
+                        pricePerDay = 130
+                        pricePerDay *= 0.88
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }else{
+                        pricePerDay = 130 
+                        if (daysOfStay < 1){
+                            console.log('Days must be positive number!')
+                            break
+                        }                      
+                        finalPrice = daysOfStay * pricePerDay
+                        console.log(`The price is ${finalPrice.toFixed(2)}lv! Have a nice time!`)
+                    }
+                    break
+                default:
+                    console.log(`Invalid input!`)
+                    break
+            }
+            break
+        default:
+            console.log(`Invalid input!`)
+            break
+    } 
 
-            }           
-            
-
-        }else if(packageType == 'noEquipment'){
-
-        }else{
-            console.log('Invalid input!')
-        }
-
-    }else if (town === 'Varna' || town === 'Burgas'){
-
-    }else{
-        console.log(`Invalid input!`)
-    }
+    
 }
+
+travelAgency(["Burgas",
+"noBreakfast",
+"no",
+"4"])
+
